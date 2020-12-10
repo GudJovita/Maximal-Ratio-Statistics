616ede1d71fae72c50b00f47503762868c473945
Jovita Gudan <58737539+jovitagudan@users.noreply.github.com>
2020-12-10 14:02:35 +0200

    Delete segment_change.R

--- segment_change.R
+++
@@ -1,8 +0,0 @@
-
-
-
-segment_change <- function(sample, subsample_index_ch, mean_break){
-  subsample_change <- sample[subsample_index_ch]+mean_break
-  sample[subsample_index_ch] <- subsample_change
-  return(sample)
-}
